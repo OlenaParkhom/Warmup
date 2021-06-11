27da8fb98300b806c0fe6bdb84d3439095b4c36a
OlenaParkhom <opa@compent.net>
2021-06-11 12:12:52 +0200

    add lines

--- src/Diff21.java
+++ src/Diff21.java
@@ -1,3 +1,15 @@
 public class Diff21 {
     public static void main(String[] args) {
+        int n = 25;
+        {
+            if (n <= 21)
+
+                System.out.print(n - 21);
+            else {
+            }
+            System.out.print((n - 21) * 2);
+
+
+        }
+    }
 }
