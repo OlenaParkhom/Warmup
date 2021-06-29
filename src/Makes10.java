cf78721d37aec1313633ec4bb9ce6e490a0cd3a9
OlenaParkhom <opa@compent.net>
2021-06-29 16:25:53 +0200

    edit Makes10

--- src/Makes10.java
+++ src/Makes10.java
@@ -1,16 +1,21 @@
 public class Makes10 {
     public static void main(String[] args) {
-        makes10();
         int a = 5;
-        int b = 6;
+        int b = 5;
+        makes10 (a,b);
     }
 
-    public boolean makes10(int a, int b){
-        return (a==10 || b==10 || a+b==10);
-    }
+    static boolean makes10(int a, int b) {
+        if (a == 10 || b == 10 || a + b == 10){
+            return true;
+        } else {
+            return false;
+        }
+
 
     }
 
+}
 
 
 
